92719b11b2d9d413ad9d4fd1b01577b87bc1cba1
Inerstellah <walkerj4@hartwick.edu>
2025-02-06 08:48:19 -0500

    work on feb 6

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,5 @@
-this is just some test text
+print("Parfois, il fait de la danse\nbien sûr, les autres gars se moquent de lui.")
+print("Il s'en fout ; il aime danser.")
+print("Une fois, il a fait un spectacle devant son école.\nTout le monde l'aimait")
+print("Après le spectacle, il a reçu une petite médaille."
+      "Il l'a portée les sept jours suivants")
