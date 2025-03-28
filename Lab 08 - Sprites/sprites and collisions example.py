bd3f5c579a9eee8778f11638fa78caca012859c6
Inerstellah <walkerj4@hartwick.edu>
2025-03-28 18:15:18 -0400

    user control + sounds

--- Lab 08 - Sprites/sprites and collisions example.py
+++ Lab 08 - Sprites/sprites and collisions example.py
@@ -1,46 +1,44 @@
+""" Sprite Sample Program """
+
 import random
 import arcade
 
+# --- Constants ---
 SPRITE_SCALING_PLAYER = 0.05
-SPRITE_SCALING_SEASHELL = 0.04
+SPRITE_SCALING_COIN = 0.04
 SEASHELL_COUNT = 50
-OBJECT_SPEED = 2
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 
 class MyGame(arcade.Window):
+    """ Our custom Window Class"""
 
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Really cool lab")
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
 
-        # these bad boys will hold sprite lists
+        # Variables that will hold sprite lists
         self.player_list = None
-        self.seashell_list = None
+        self.coin_list = None
 
+        # Set up the player info
         self.player_sprite = None
         self.score = 0
 
+        # Don't show the mouse cursor
         self.set_mouse_visible(False)
 
-        arcade.set_background_color(arcade.csscolor.NAVY)
-
-    class Seashell:
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-            self.player_sprite = arcade.Sprite("seashell.png", SPRITE_SCALING_SEASHELL)
-            self.player_sprite.center_x = self.x
-            self.player_sprite.center_y = self.y
+        arcade.set_background_color(arcade.color.AMAZON)
 
     def setup(self):
-
         """ Set up the game and initialize the variables. """
 
         # Sprite lists
         self.player_list = arcade.SpriteList()
-        self.seashell_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
 
         # Set up the player
         # Character image from kenney.nl
@@ -49,22 +47,24 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
-        # Create the seashells
+        # Create the coins
         for i in range(SEASHELL_COUNT):
-            # Create the seashell instance
-            seashell = arcade.Sprite("seashell.png", SPRITE_SCALING_SEASHELL)
 
-            # Position the seashell
-            seashell.center_x = random.randrange(SCREEN_WIDTH)
-            seashell.center_y = random.randrange(SCREEN_HEIGHT)
+            # Create the coin instance
+            # Coin image from kenney.nl
+            coin = arcade.Sprite("seashell.png", SPRITE_SCALING_COIN)
 
-            # Add the seashell to the list
-            self.seashell_list.append(seashell)
+            # Position the coin
+            coin.center_x = random.randrange(SCREEN_WIDTH)
+            coin.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # Add the coin to the lists
+            self.coin_list.append(coin)
 
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
-        self.seashell_list.draw()
+        self.coin_list.draw()
         self.player_list.draw()
 
         # Put the text on the screen.
@@ -83,18 +83,15 @@ class MyGame(arcade.Window):
 
         # Call update on all sprites (The sprites don't do much in this
         # example though.)
-        self.seashell_list.update()
+        self.coin_list.update()
 
         # Generate a list of all sprites that collided with the player.
-        seashells_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
-                                                              self.seashell_list)
-
-        for seashell in self.seashell_list:
-            seashell.center_x += OBJECT_SPEED
+        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                              self.coin_list)
 
         # Loop through each colliding sprite, remove it, and add to the score.
-        for seashell in seashells_hit_list:
-            seashell.remove_from_sprite_lists()
+        for coin in coins_hit_list:
+            coin.remove_from_sprite_lists()
             self.score += 1
 
 
@@ -103,6 +100,7 @@ def main():
     window = MyGame()
     window.setup()
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
