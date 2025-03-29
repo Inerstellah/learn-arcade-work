6c310a68e024472c9594d35528a9a50c10ee46a4
Inerstellah <walkerj4@hartwick.edu>
2025-03-29 18:54:36 -0400

    user control + sounds

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -2,9 +2,12 @@ import random
 import arcade
 
 SPRITE_SCALING_PLAYER = 0.05
-SPRITE_SCALING_SEASHELL = 0.04
+SPRITE_SCALING_SEASHELL = 0.05
+SPRITE_SCALING_ROCK = 0.4
 SEASHELL_COUNT = 50
+ROCK_COUNT = 25
 OBJECT_SPEED = 2
+PLAYING = True
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -18,6 +21,7 @@ class MyGame(arcade.Window):
         # these bad boys will hold sprite lists
         self.player_list = None
         self.seashell_list = None
+        self.rock_list = None
 
         self.player_sprite = None
         self.score = 0
@@ -27,12 +31,16 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.csscolor.NAVY)
 
     class Seashell:
-        def __init__(self, x, y):
+        def __init__(self, x):
             self.x = x
+            self.seashell_sprite = arcade.Sprite("seashell.png", SPRITE_SCALING_SEASHELL)
+            self.seashell_sprite.center_x = self.x
+
+    class Rock:
+        def __init__(self, y):
             self.y = y
-            self.player_sprite = arcade.Sprite("seashell.png", SPRITE_SCALING_SEASHELL)
-            self.player_sprite.center_x = self.x
-            self.player_sprite.center_y = self.y
+            self.rock_sprite = arcade.Sprite("real-rock", SPRITE_SCALING_SEASHELL)
+            self.rock_sprite.center_y = self.y
 
     def setup(self):
 
@@ -41,9 +49,9 @@ class MyGame(arcade.Window):
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.seashell_list = arcade.SpriteList()
+        self.rock_list = arcade.SpriteList()
 
         # Set up the player
-        # Character image from kenney.nl
         self.player_sprite = arcade.Sprite("crab.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
@@ -58,18 +66,37 @@ class MyGame(arcade.Window):
             seashell.center_x = random.randrange(SCREEN_WIDTH)
             seashell.center_y = random.randrange(SCREEN_HEIGHT)
 
-            # Add the seashell to the list
+            # Add the seashells to their list
             self.seashell_list.append(seashell)
+
+        for i in range(ROCK_COUNT):
+            # create rock instance
+            rock = arcade.Sprite("real-rock.png", SPRITE_SCALING_ROCK)
+
+            # position the rocks
+            rock.center_x = random.randrange(SCREEN_WIDTH)
+            rock.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # add the rocks to their list
+            self.rock_list.append(rock)
 
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
         self.seashell_list.draw()
         self.player_list.draw()
+        self.rock_list.draw()
 
         # Put the text on the screen.
         output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+        if len(self.seashell_list) < 1:
+            arcade.draw_text("You collected all the seashells! Game over!",
+                             150, 300, arcade.color.BLACK, 22)
+            global OBJECT_SPEED, PLAYING
+            OBJECT_SPEED = 0
+            PLAYING = False
 
     def on_mouse_motion(self, x, y, dx, dy):
         """ Handle Mouse Motion """
@@ -88,14 +115,32 @@ class MyGame(arcade.Window):
         # Generate a list of all sprites that collided with the player.
         seashells_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
                                                               self.seashell_list)
+        rocks_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                              self.rock_list)
 
         for seashell in self.seashell_list:
             seashell.center_x += OBJECT_SPEED
+            if seashell.center_x >= SCREEN_WIDTH + 25:
+                seashell.center_x = -60
+
+        for rock in self.rock_list:
+            rock.center_y -= OBJECT_SPEED
+            if rock.center_y <= -45:
+                rock.center_y = SCREEN_HEIGHT + 45
 
         # Loop through each colliding sprite, remove it, and add to the score.
-        for seashell in seashells_hit_list:
-            seashell.remove_from_sprite_lists()
-            self.score += 1
+        if PLAYING:
+            for seashell in seashells_hit_list:
+                seashell.remove_from_sprite_lists()
+                self.score += 1
+
+            for rock in rocks_hit_list:
+                rock.remove_from_sprite_lists()
+                self.score -= 1
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.SPACE:
+            arcade.close_window()
 
 
 def main():
