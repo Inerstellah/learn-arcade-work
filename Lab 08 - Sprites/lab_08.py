bd95a1daf64070e8968d5a29ba96ed060741b9c3
Inerstellah <walkerj4@hartwick.edu>
2025-03-30 17:26:16 -0400

    user control + sounds

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -3,10 +3,15 @@ import arcade
 
 SPRITE_SCALING_PLAYER = 0.05
 SPRITE_SCALING_SEASHELL = 0.05
+SPRITE_SCALING_CAT = 0.2
 SPRITE_SCALING_ROCK = 0.4
 SEASHELL_COUNT = 50
 ROCK_COUNT = 25
+CAT_COUNT = 3
 OBJECT_SPEED = 2
+CAT_SPEED = 15
+CATS_HIT = 0
+CAT_TIMES_TRAVELED = 0
 PLAYING = True
 
 SCREEN_WIDTH = 800
@@ -22,6 +27,7 @@ class MyGame(arcade.Window):
         self.player_list = None
         self.seashell_list = None
         self.rock_list = None
+        self.cat_list = None
 
         self.player_sprite = None
         self.score = 0
@@ -39,8 +45,14 @@ class MyGame(arcade.Window):
     class Rock:
         def __init__(self, y):
             self.y = y
-            self.rock_sprite = arcade.Sprite("real-rock", SPRITE_SCALING_SEASHELL)
+            self.rock_sprite = arcade.Sprite("real-rock.png", SPRITE_SCALING_SEASHELL)
             self.rock_sprite.center_y = self.y
+
+    class Cat:
+        def __init__(self, x):
+            self.x = x
+            self.cat_sprite = arcade.Sprite("CAT.png", SPRITE_SCALING_CAT)
+            self.cat_sprite_center = x
 
     def setup(self):
 
@@ -50,6 +62,7 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.seashell_list = arcade.SpriteList()
         self.rock_list = arcade.SpriteList()
+        self.cat_list = arcade.SpriteList()
 
         # Set up the player
         self.player_sprite = arcade.Sprite("crab.png", SPRITE_SCALING_PLAYER)
@@ -80,12 +93,24 @@ class MyGame(arcade.Window):
             # add the rocks to their list
             self.rock_list.append(rock)
 
+        for i in range(CAT_COUNT):
+            # create rock instance
+            cat = arcade.Sprite("CAT.png", SPRITE_SCALING_CAT)
+
+            # position the rocks
+            cat.center_x = random.randrange(SCREEN_WIDTH)
+            cat.center_y = random.randrange(SCREEN_HEIGHT)
+
+            # add the rocks to their list
+            self.cat_list.append(cat)
+
     def on_draw(self):
         """ Draw everything """
         arcade.start_render()
         self.seashell_list.draw()
         self.player_list.draw()
         self.rock_list.draw()
+        self.cat_list.draw()
 
         # Put the text on the screen.
         output = f"Score: {self.score}"
@@ -93,7 +118,11 @@ class MyGame(arcade.Window):
 
         if len(self.seashell_list) < 1:
             arcade.draw_text("You collected all the seashells! Game over!",
-                             150, 300, arcade.color.BLACK, 22)
+                             115, 300, arcade.color.BLACK, 22)
+            arcade.draw_text("You also collected " + str(CATS_HIT) + " of 3 cats!",
+                             205, 250, arcade.color.BLACK, 22)
+            arcade.draw_text("You can hit space to close btw",
+                             203, 200, arcade.color.BLACK, 22)
             global OBJECT_SPEED, PLAYING
             OBJECT_SPEED = 0
             PLAYING = False
@@ -117,6 +146,8 @@ class MyGame(arcade.Window):
                                                               self.seashell_list)
         rocks_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
                                                               self.rock_list)
+        cats_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                             self.cat_list)
 
         for seashell in self.seashell_list:
             seashell.center_x += OBJECT_SPEED
@@ -128,6 +159,13 @@ class MyGame(arcade.Window):
             if rock.center_y <= -45:
                 rock.center_y = SCREEN_HEIGHT + 45
 
+        for cat in self.cat_list:
+            global CAT_TIMES_TRAVELED
+            cat.center_x += CAT_SPEED
+            if cat.center_x >= SCREEN_WIDTH + 60 and CAT_TIMES_TRAVELED < 15:
+                cat.center_x = -60
+                CAT_TIMES_TRAVELED += 1
+
         # Loop through each colliding sprite, remove it, and add to the score.
         if PLAYING:
             for seashell in seashells_hit_list:
@@ -137,6 +175,12 @@ class MyGame(arcade.Window):
             for rock in rocks_hit_list:
                 rock.remove_from_sprite_lists()
                 self.score -= 1
+
+            for cat in cats_hit_list:
+                cat.remove_from_sprite_lists()
+                self.score += 5
+                global CATS_HIT
+                CATS_HIT += 1
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.SPACE:
